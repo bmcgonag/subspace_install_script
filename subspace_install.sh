8d5283924fa603c839306afaaab6205a848c9da1
Brian McGonagill <bmcgonag@gmail.com>
2019-11-14 10:38:57 -0600

    fixed mispelling for docker repo setup.

--- subspace_install.sh
+++ subspace_install.sh
@@ -57,7 +57,7 @@ echo ''
 echo ''
 echo 'Install Docker'
 sleep 1
-apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-commonocker
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
